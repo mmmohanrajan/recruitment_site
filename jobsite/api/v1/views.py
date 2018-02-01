10040146764afb320315979e90e87539736dee0c
Mohanrajan <mmmohanrajan@gmail.com>
2018-02-01 16:11:28 +0530

    Added docs/

--- jobsite/api/v1/views.py
+++ jobsite/api/v1/views.py
@@ -10,6 +10,13 @@ from api.v1.serializers import JobPostSerializer, EmployeeSerializer
 
 
 class OpeningsAPIView(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all the existing Openings.
+
+    post:
+    Create a new Openings instance.
+    """
     queryset = JobPost.objects.all()  
     serializer_class = JobPostSerializer
 
@@ -17,20 +24,45 @@ class OpeningsAPIView(generics.ListCreateAPIView):
         serializer.save(posted_by=self.request.user)
 
 class OpeningsDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """
+    get:
+    Return a single openings details.
+
+    update:
+    Update opeings instant.
+
+    delete:
+    Delete opeings instant.
+    """
     queryset = JobPost.objects.all()  
     serializer_class = JobPostSerializer
         
 
 class ApplicationAPIView(generics.ListCreateAPIView):
+    """
+    get:
+    Return a list of all the existing Applications.
+
+    post:
+    Create a new Application instance.
+    """    
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
     permission_classes = (AllowAny,)
 
 class ApplicationDetailAPIView(generics.RetrieveAPIView):
+    """
+    get:
+    Return a details about single Application instant.
+    """
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
 class ApplicationAcceptAPIView(APIView):
+    """
+    patch:
+    Used to make application accept or reject.
+    """
     def get_object(self, pk):
         return Employee.objects.get(pk=pk)
 
